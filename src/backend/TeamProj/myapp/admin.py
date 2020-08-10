6016b8ed3e4cb5b8fc933a99a60d7c45b822d742
DbettKK <394739138@qq.com>
2020-08-10 16:17:28 +0800

    增加团队表、成员表、用户收藏、用户浏览表

--- src/backend/TeamProj/myapp/admin.py
+++ src/backend/TeamProj/myapp/admin.py
@@ -6,3 +6,8 @@ from . import models
 admin.site.register(models.User)
 admin.site.register(models.File)
 admin.site.register(models.Comment)
+admin.site.register(models.Team)
+admin.site.register(models.TeamMember)
+admin.site.register(models.UserBrowseFile)
+admin.site.register(models.UserKeptFile)
+
--- src/backend/TeamProj/myapp/models.py
+++ src/backend/TeamProj/myapp/models.py
@@ -9,9 +9,25 @@ class User(models.Model):
     username = models.CharField(max_length=32, verbose_name='用户名', unique=True)
     password = models.CharField(max_length=128, verbose_name='password')
     phone_num = models.CharField(max_length=11, verbose_name='电话')
-    email = models.EmailField()
-    create_time = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField(unique=True)
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name='用户注册时间')
     isActive = models.BooleanField()
+
+    browse = models.ManyToManyField(
+        'File',
+        through='UserBrowseFile',
+        through_fields=('person', 'file'),
+        verbose_name='最近浏览',
+        related_name='browse'
+    )
+
+    kept = models.ManyToManyField(
+        'File',
+        through='UserKeptFile',
+        through_fields=('person', 'file'),
+        verbose_name='收藏',
+        related_name='kept'
+    )
 
     def __str__(self):
         return self.username
@@ -39,26 +55,36 @@ class File(models.Model):
     team_permissions = (
         (1, '成员可查看'), (2, '成员可评论'), (3, '成员可修改'), (4, '成员可分享')
     )
+    types = (('team', '团队文档'), ('private', '私人文档'))
     file_name = models.CharField(max_length=64, verbose_name='文档名')
     file_title = models.CharField(max_length=64, verbose_name='文档标题')
     file_content = models.CharField(max_length=128, verbose_name='文档内容')
     create_time = models.DateTimeField(auto_now_add=True, verbose_name='文档创建时间')
     last_modified = models.DateTimeField(auto_now=True, verbose_name='文档最后一次修改时间')
+    type = models.CharField(max_length=32, choices=types, verbose_name='文档类型')
     permission = models.IntegerField(choices=permissions, verbose_name='总权限')
-    team_permission = models.IntegerField(choices=team_permissions, verbose_name='团队内部权限')
+    team_permission = models.IntegerField(choices=team_permissions, verbose_name='团队内部权限', null=True)
     creator = models.ForeignKey(
         'User',
         on_delete=models.CASCADE,
-        related_name='myfile',
+        related_name='myfiles',
         verbose_name='文档创建者'
     )
     comments = models.ManyToManyField(
         'User',
         through='Comment',
         through_fields=('file', 'person'),
-        verbose_name='评论'
+        verbose_name='评论',
     )
-    share = models.CharField(max_length=64, verbose_name='分享')
+    team_belong = models.ForeignKey(
+        'Team',
+        on_delete=models.CASCADE,
+        related_name='teamfiles',
+        verbose_name='所属团队',
+        null=True
+    )
+
+    share = models.CharField(max_length=64, verbose_name='分享', null=True)
 
 
 class Comment(models.Model):
@@ -75,3 +101,36 @@ class Comment(models.Model):
     content = models.CharField(max_length=128, verbose_name='评论内容')
     create_time = models.DateTimeField(auto_now_add=True, verbose_name='评论创建时间')
 
+
+class Team(models.Model):
+    creator = models.ForeignKey('User', on_delete=models.CASCADE, related_name='team_own', verbose_name='团队创建者')
+    members = models.ManyToManyField(
+        'User',
+        through='TeamMember',
+        through_fields=('team', 'member'),
+        verbose_name='团队成员',
+    )
+    name = models.CharField(max_length=32, verbose_name='团队名称')
+
+
+class TeamMember(models.Model):
+    permissions = (
+        (1, '可查看'), (2, '可评论'), (3, '可修改'), (4, '可分享')
+    )
+
+    team = models.ForeignKey('Team', on_delete=models.CASCADE, verbose_name='所属团队')
+    member = models.ForeignKey('User', on_delete=models.CASCADE, verbose_name='团队成员')
+    join_time = models.DateTimeField(auto_now_add=True, verbose_name='加入时间')
+    permission = models.IntegerField(choices=permissions, verbose_name='成员权限')
+
+
+class UserBrowseFile(models.Model):
+    file = models.ForeignKey('File', on_delete=models.CASCADE, verbose_name='浏览的文档')
+    person = models.ForeignKey('User', on_delete=models.CASCADE, verbose_name='浏览的用户')
+    last_modified = models.DateTimeField(auto_now=True, verbose_name='最近一次浏览时间')
+
+
+class UserKeptFile(models.Model):
+    file = models.ForeignKey('File', on_delete=models.CASCADE, verbose_name='收藏的文档')
+    person = models.ForeignKey('User', on_delete=models.CASCADE, verbose_name='收藏的用户')
+    kept_time = models.DateTimeField(auto_now_add=True, verbose_name='收藏时间')
