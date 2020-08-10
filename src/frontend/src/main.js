a2d3594efff7b7c16d3e162193799c7e2cf5dd5c
笪昊凡 <1374109711@qq.com>
2020-08-10 17:22:36 +0800

    home页面，welcome页面，axios请求拦截，路由导航守卫
    路由导航守卫暂时没有考虑注册页面！

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -10,6 +10,12 @@ import 'element-ui/lib/theme-chalk/index.css';
 import qs from 'qs';
 
 axios.defaults.baseURL = 'http://175.24.121.113:8000/myapp/register'
+//axios请求拦截
+axios.interceptors.request.use(config => {
+  console.log (config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 Vue.prototype.$http = axios
 Vue.prototype.$qs = qs;
 
