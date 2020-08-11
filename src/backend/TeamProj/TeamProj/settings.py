7d9494ddb2510def4aecb64230e3962b44ae291d
haosheng055 <54499706+haosheng055@users.noreply.github.com>
2020-08-11 16:29:21 +0800

    setting.py修复

--- src/backend/TeamProj/TeamProj/settings.py
+++ src/backend/TeamProj/TeamProj/settings.py
@@ -96,8 +96,12 @@ WSGI_APPLICATION = 'TeamProj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'teamproj',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
