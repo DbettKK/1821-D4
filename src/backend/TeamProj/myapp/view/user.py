833962e888ab5d913789569d480bd11ed8b80f38
DbettKK <394739138@qq.com>
2020-08-11 13:56:43 +0800

    小改动

--- src/backend/TeamProj/myapp/view/user.py
+++ src/backend/TeamProj/myapp/view/user.py
@@ -64,13 +64,13 @@ class UserRegister(APIView):
                 'code': 400,
                 'registered': False,
             }, status=400)
-        if(User.objects.fiter(email=email).count()!=0) :
+        if User.objects.fiter(email=email).count()!=0:
             return Response({
                 'info': 'emailExist',
                 'code': 403,
                 'registered': False,
             }, status=403)
-        if(User.objects.fiter(username=username).count()!=0) :
+        if User.objects.fiter(username=username).count()!=0:
             return Response({
                 'info': 'usernameExist',
                 'code': 403,
